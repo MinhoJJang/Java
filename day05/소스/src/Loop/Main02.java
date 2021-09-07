41337fae494b4d41b149c9aae06e559a2d306709
Minho Jang <84257033+MinhoJJang@users.noreply.github.com>
2021-09-07 21:41:51 +0900

    Add files via upload

---
+++ day05/소스/src/Loop/Main02.java
@@ -0,0 +1,18 @@
+package Loop;
+
+public class Main02 {
+
+	public static void main(String[] args) {
+		// 구구단 7단
+		int j = 0;
+		for(int i = 1; i < 10; i++) {
+			j = 7 * i;
+			System.out.println("7 * " + i + " = " + j);
+		}
+		
+		
+		
+		
+	}
+
+}
