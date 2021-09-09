785fab25492e848c62366dbbf3ba4ee98e6ae84f
Minho Jang <84257033+MinhoJJang@users.noreply.github.com>
2021-09-10 00:50:43 +0900

    Add files via upload

---
+++ day07/소스코드/src/Method/Main01.java
@@ -0,0 +1,18 @@
+package Method;
+
+public class Main01 {
+
+	public static void main(String[] args) {
+		f();
+		f();
+		f();
+		f();
+	}
+
+	public static void f() {
+		int x = 100;
+		int y = x + 1;
+		System.out.println(y);
+	}
+	
+}
